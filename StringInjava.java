6ddb11f7c659143335fe0f4c0441b4ced5982483
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-08 04:43:52 +0500

    Create StringInjava.java

---
+++ StringInjava.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class StringInJava {
+	
+	public  static void main(String[] args) {
+//		String name = new String("Mubashir");
+//		String name = " Mubashir";
+//		System.out.print("The name is :");
+//		System.out.print(name);
+		
+		
+		
+//		int a = 6;
+//		float b = 5.9808f;
+//		System.out.printf("The Value of a is %d and value of b is %f", a,b);
+//		System.out.format("The Value of a is %d and value of b is %f", a,b);
+		Scanner sc = new Scanner(System.in);
+//		String st = sc.next();
+		String st = sc.nextLine();
+
+		System.out.println(st);
+
+	
+	}
+
+}

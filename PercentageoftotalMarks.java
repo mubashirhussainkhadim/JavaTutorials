c33293d6fbd2ea94b53609aae88ae792159ae708
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-07 23:51:46 +0500

    Update PercentageoftotalMarks.java

--- PercentageoftotalMarks.java
+++ PercentageoftotalMarks.java
@@ -17,13 +17,13 @@ public class Marksper{
         int cm = sc.nextInt();
         System.out.println("Total Marks obtain of the Islamiyat");
         int Is = sc.nextInt();
-        int TotalMarks = 500;
         int TotalMarksObtain = m+p+c+cm+Is;
 		System.out.println("Total Marks Obtain");
 		System.out.println(TotalMarksObtain);
-		int MultiplyObtainMarksBy100 =  TotalMarksObtain*100;
-		float percentage = MultiplyObtainMarksBy100/TotalMarks;
+		float percentage = (TotalMarksObtain*100)/500; 
 		System.out.println("percentage");
 		System.out.println(percentage);
+		
+
 	}
-}
+

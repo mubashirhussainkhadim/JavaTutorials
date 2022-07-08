05650123ff79870ae7c475c47be8b653f2f20cf3
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-08 22:33:17 +0500

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main (String[] args) {
+    	System.out.println(1);
+    	System.out.println(2);
+    	System.out.println(3);
+    	int i = 1 ;
+    	System.out.println("Using Loops");
+    	while (i <= 3) {
+        	System.out.println(i++);
+
+    	}
+    }
+}

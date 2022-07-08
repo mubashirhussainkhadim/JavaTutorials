6f092698ab79ec0a91abd62e4c541f1203c8421f
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-09 02:17:16 +0500

    Create Countinue.java

---
+++ Countinue.java
@@ -0,0 +1,12 @@
+
+public class Countinue {
+   public static void main(String[] args){
+	   for (int i =7;  i > 0; i --) {
+		   if (i == 3) {
+			   continue;
+		   }
+		   System.out.println("the value : " + i);
+	   }
+   }
+   
+}

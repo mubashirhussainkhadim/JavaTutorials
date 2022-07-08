2e8650aa6a7deec8c60d1038b9307938f9c3f703
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-09 02:17:57 +0500

    Create Break.java

---
+++ Break.java
@@ -0,0 +1,12 @@
+
+public class Break {
+   public static void main(String[] args){
+	   for (int i =10;  i > 0; i --) {
+		   if (i == 7) {
+			   break;
+		   }
+		   System.out.println("the value : " + i);
+	   }
+   }
+   
+}
